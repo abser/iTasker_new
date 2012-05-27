76c70e24d7885e0847a5f7721a54792736a16479
Nurul Abser <abser78@Nuruls-MacBook-Pro.local>
2012-05-27 11:41:38 +0800

    Initial Commit

---
+++ iTasker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iTasker
+//
+//  Created by Nurul Abser on 27/5/12.
+//  Copyright (c) 2012 abser@zingmobile.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ iTasker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iTasker
+//
+//  Created by Nurul Abser on 27/5/12.
+//  Copyright (c) 2012 abser@zingmobile.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
